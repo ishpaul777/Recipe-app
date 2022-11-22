28301431996fbdbff9b16ec1f786e162fd839778
looh16 <langacustodio@gmail.com>
2022-11-22 22:37:25 +0200

    Add tests for food views

--- spec/requests/foods/new.html.erb_spec.rb
+++ spec/requests/foods/new.html.erb_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe 'Foods', type: :request do
   let(:user) { User.create(name: 'Jhon', email: 'Jhon@mail.com', password: '123456') }
   let(:food) { user.foods.create(name: 'Banana', measurement_unit: 'Kg', price: 20) }
 
-  describe 'GET /index' do
+  describe 'GET /new' do
     before do
       sign_in user
-      get foods_path
+      get new_food_path
     end
 
     it 'should return status correct (ok)' do
