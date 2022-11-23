47c1e0f2e9fb7d7b473e5a2f1418aca3f8c1d289
looh16 <langacustodio@gmail.com>
2022-11-23 09:55:10 +0200

    Add tests for recipe foods views

--- spec/requests/recipe_foods/new.html.erb_spec.rb
+++ spec/requests/recipe_foods/new.html.erb_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'Foods', type: :request do
+RSpec.describe 'recipe_foods', type: :request do
   include Devise::Test::IntegrationHelpers
 
   let(:user) { User.create(name: 'Jhon', email: 'Jhon@mail.com', password: '123456') }
   let(:food) { user.foods.create(name: 'Banana', units: 'Kg', price: 20) }
+  let(:recipe) do
+    user.recipes.create(name: 'cake', preparation_time: 1, cooking_time: 10, description: 'cake', public: true,
+                        user_id: user.id)
+  end
 
   describe 'GET /new' do
     before do
       sign_in user
-      get new_food_path
+      get new_recipe_recipe_food_path(recipe)
     end
 
     it 'should return status correct (ok)' do
