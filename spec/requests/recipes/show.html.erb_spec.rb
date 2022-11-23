aa95384df2d9cdbc5e84b9732e564afd4c1f3341
looh16 <langacustodio@gmail.com>
2022-11-23 10:49:43 +0200

    Add recipes views

--- spec/requests/recipes/show.html.erb_spec.rb
+++ spec/requests/recipes/show.html.erb_spec.rb
@@ -4,11 +4,16 @@ RSpec.describe 'Recipes', type: :request do
   include Devise::Test::IntegrationHelpers
 
   let(:user) { User.create(name: 'Jhon', email: 'Jhon@mail.com', password: '123456') }
+  let(:food) { user.foods.create(name: 'Banana', units: 'Kilos', price: 20, quantity: 1) }
+  let(:recipe) do
+    user.recipes.create(name: 'cake', preparation_time: 1, cooking_time: 10, description: 'cake taste', public: true,
+                        user_id: user.id)
+  end
 
-  describe 'GET /public' do
+  describe 'GET /show' do
     before do
       sign_in user
-      get public_recipes_path
+      get recipe_path(recipe)
     end
 
     it 'should return status correct (ok)' do
